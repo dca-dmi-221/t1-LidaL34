dc0b1f444996f1e5e9b4ef889cdc372c20207ede
LidaL34 <marce001113@hotmail.com>
2022-03-30 19:14:58 -0500

    Avances

--- sketch.js
+++ sketch.js
@@ -1,18 +1,63 @@
 const songsData= [
   {
-    name: "Cancion 1",
-    file: "/songs/file.mp3",
-    img: "/img/imagen.jpg"
+    name: "All Of You",
+    file: "/Musica/All Of You.mp3",
+    img: "/Imagenes/Encanto.jpg"
   },
   {
-    name: "Cancion 1",
-    file: "/songs/file.mp3",
-    img: "/img/imagen.jpg"
+    name: "Dos Orugitas",
+    file: "/Musica/Dos Orugitas.mp3",
+    img: "/Imagenes/Encanto.jpg"
   },
   {
-    name: "Cancion 1",
-    file: "/songs/file.mp3",
-    img: "/img/imagen.jpg"
+    name: "Gotta Go My Own Way",
+    file: "/Musica/Gotta Go My Own Way.mp3",
+    img: "/Imagenes/HSM.jpg"
+  },
+  {
+    name: "How Far Ill Go",
+    file: "/Musica/How Far Ill Go.mp3",
+    img: "/Imagenes/Moana.jpg"
+  },
+  {
+    name: "Let It Go",
+    file: "/Musica/Let It Go.mp3",
+    img: "/Imagenes/HSM.jpg"
+  },
+  {
+    name: "Rewrite The Stars",
+    file: "/Musica/Rewrite The Stars.mp3",
+    img: "/Imagenes/TGSM.jpg"
+  },
+  {
+    name: "Surface Pressure",
+    file: "/Musica/Surface Pressure.mp3",
+    img: "/Imagenes/Encanto.jpg"
+  },
+  {
+    name: "The Family Madrigal",
+    file: "/Musica/The Family Madrigal.mp3",
+    img: "/Imagenes/Encanto.jpg"
+  },
+  {
+    name: "Waiting On A Miracle",
+    file: "/Musica/Waiting On A Miracle.mp3",
+    img: "/Imagenes/Encanto.jpg"
+  },
+  {
+    name: "We Dont Talk About Bruno",
+    file: "/Musica/We Dont Talk About Bruno.mp3",
+    img: "/Imagenes/Encanto.jpg"
+  },
+  {
+    name: "What Else Can I Do",
+    file: "/Musica/What Else Can I Do.mp3",
+    img: "/Imagenes/Encanto.jpg"
+  },
+  {
+    name: "When Will My Life Begin",
+    file: "/Musica/When Will My Life Begin.mp3",
+    img: "/Imagenes/Tangle.jpg"
   }
 ];
 let songFiles = [];
@@ -34,4 +79,12 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function mouseClick() {
+  app.click();
+}
+
+function mousePress() {
+  app.press();
 }
